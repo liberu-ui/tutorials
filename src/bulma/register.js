7a1742c28350570183539e1e2824d21253181db6
Curtis Delicata <curtis.delicata@outlook.com>
2023-10-06 16:01:04 +0000

    Replace enso-ui to liberu-ui

--- src/bulma/register.js
+++ src/bulma/register.js
@@ -1,4 +1,4 @@
-import App from '@enso-ui/ui/src/core/app';
+import App from '@liberu-ui/ui/src/core/app';
 import Tutorial from './components/settings/Tutorial.vue';
 
 App.registerSettingsItem('tutorials',  Tutorial, 350);
